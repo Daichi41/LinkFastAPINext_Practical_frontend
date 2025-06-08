17dc0fd15e0b3fc440cc136a7267548dd612b76b
Daichi41 <h.daichi.6@gmail.com>
2025-06-08 21:07:22 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,12 +2,9 @@ require('dotenv').config()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
+        // Reference a variable that was defined in the .env file and make it available at Build Time
         API_ENDPOINT: process.env.API_ENDPOINT,
-    },
-    // 本番環境でのみ standalone を有効化
-    ...(process.env.NODE_ENV === 'production' && {
-        output: 'standalone'
-    }),
+      },
 }
 
 module.exports = nextConfig
