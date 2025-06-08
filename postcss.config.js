8b9f25393080bcdf9c5240f39db36577d0a7e564
Daichi41 <h.daichi.6@gmail.com>
2025-06-08 20:45:33 +0900

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+/** 修正後（standalone機能を追加）*/
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'standalone'
+      }
+
+module.exports = nextConfig
